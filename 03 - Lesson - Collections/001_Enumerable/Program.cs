f42531be6d1d0cbf0209719070d9ec688d2825c4
artonoyan <artonoyan@servicetitan.com>
2021-01-18 20:26:18 +0400

    Add new solution Lesson_Collections

---
+++ 03 - Lesson - Collections/001_Enumerable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _001_Enumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode node1 = new ListNode { value = 10 };
+            ListNode node2 = new ListNode { value = 20 };
+            node1.next = node2;
+            ListNode node3 = new ListNode { value = 30 };
+            node2.next = node3;
+            ListNode node4 = new ListNode { value = 40 };
+            node3.next = node4;
+        }
+    }
+
+    class ListNode
+    {
+        public int value;
+        public ListNode next;
+
+        public override string ToString() => value.ToString();
+    }
+}
