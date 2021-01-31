10d48ff4db092cecf74f86fc2db5eba97fbcef28
artonoyan <artonoyan@servicetitan.com>
2021-01-31 18:39:10 +0400

    [Expressions Part1] Add new lesson

---
+++ 05 - Lesson - Expressions Part1/010_Expressions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace _010_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var st = new Student { Name = "A1", Surname = "A1yan", Age = 15 };
+
+            Expression<Func<Student, bool>> q1 = s => s.Age > 12;
+            Expression<Func<Student, bool>> q2 = s => s.Age < 20;
+
+            Expression<Func<Student, bool>> isTeenAgerExpr = q1.And(q2);
+
+            Func<Student, bool> isTeenAger = isTeenAgerExpr.Compile();
+            if (isTeenAger(st))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
