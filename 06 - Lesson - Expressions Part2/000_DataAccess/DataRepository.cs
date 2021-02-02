ed4b311742419c00e89e0e16a104fc0f133c907b
artonoyan <artonoyan@servicetitan.com>
2021-02-03 00:07:08 +0400

    [Expressions] Add new lesson.

---
+++ 06 - Lesson - Expressions Part2/000_DataAccess/DataRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _000_DataAccess
+{
+    public class DataRepository
+    {
+        public DataRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private readonly string _connectionString;
+
+        public IEnumerable<TModel> Execute<TModel>(string query, Func<IDataRecord, TModel> convert)
+        {
+            foreach (var reader in AsEnumerable(query))
+            {
+                yield return convert(reader);
+            }
+        }
+
+        public IEnumerable<IDataRecord> AsEnumerable(string query)
+        {
+            using var conn = new SqlConnection(_connectionString);
+            conn.Open();
+
+            using var comand = conn.CreateCommand();
+            comand.CommandType = CommandType.Text;
+            comand.CommandText = query;
+            var reader = comand.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                    yield return reader;
+            }
+        }
+    }
+}
---
+++ 06 - Lesson - Expressions Part2/009_Expressions/Program.cs
@@ -0,0 +1,33 @@
+﻿using _000_DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _009_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = Stopwatch.StartNew();
+
+            List<Blog> blogs1 = Dapper.Execute<Blog>("select * from Blog").ToList();
+            Console.WriteLine(sw.Elapsed);
+            sw = Stopwatch.StartNew();
+
+            List<Blog> blogs2 = Dapper.Execute<Blog>("select * from Blog").ToList();
+            Console.WriteLine(sw.Elapsed);
+            sw = Stopwatch.StartNew();
+
+            List<Blog> blogs3 = Dapper.Execute<Blog>("select * from Blog").ToList();
+            Console.WriteLine(sw.Elapsed);
+            sw = Stopwatch.StartNew();
+
+            List<Blog> blogs4 = Dapper.Execute<Blog>("select * from Blog").ToList();
+            Console.WriteLine(sw.Elapsed);
+
+            Console.ReadLine();
+        }
+    }
+}
